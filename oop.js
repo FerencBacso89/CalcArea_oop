ecd4aaa94e5b2246950b0e08bcc8326b11f10c37
Bacsó Ferenc <50657103+FerencBacso89@users.noreply.github.com>
2023-08-05 20:30:59 +0200

    making class concept objects
    calculation works for 3 geometries

--- oop.js
+++ oop.js
@@ -10,87 +10,135 @@
 // egységbe zárás           Képletek  https://www.calculat.org/hu/terulet-kerulet/
 const parA = document.getElementById("paramA");
 const parB = document.getElementById("paramB");
-
+const parC = document.getElementById("paramC");
+const parH = document.getElementById("paramH");
+let  selectedGeoId;
 class acientGeo {
-   constructor(a,b,c,m){
+   constructor(a,b,c,h){
       this.a=a;   //a side
       this.b=b;   //b side
       this.c=c;   //c side
-      this.m=m;   //m Height
+      this.h=h;   //h Height
    }
    getA() {return this.a}
    getB() {return this.b}
    getC() {return this.c}
-   getM() {return this.m}
+   geth() {return this.h}
 }
 class square extends acientGeo{
   constructor(a){
       super(a,a);
+      this.a=parseFloat(document.getElementById("aSide").value);
    }
-   getRectDiag() {return Math.sqrt(2)*this.a}   //átló
-   getRectArea() {return Math.pow(this.a,2)}   //terület
-   getRectPerim() {return (4*this.a)}   //kerület
+   getSqDiag() {return Math.sqrt(2)*this.a}   //átló
+   getSqArea() {return Math.pow(this.a,2)}   //terület
+   getSqPerim() {return (4*this.a)}   //kerület
 }
 class rectangle extends acientGeo{
-   constructor(a,b){
-      super(a,b);
+   constructor(a){
+      super();
+      this.a=a;
+      this.b= parseFloat(document.getElementById("bSide").value);
    }         
-   getSqArea() {return this.a*this.b}   //terület
-   getSqPerim() {return (2*(this.a+this.b))}   //kerület
+   getRectArea() {return a*this.b}   //terület
+   getRectPerim() {return (2*(a+this.b))}   //kerület
+}
+class trinagle extends acientGeo{
+   constructor(a,b,c,h){
+      super();
+      this.a=a;
+      this.b=b;
+      this.c=parseFloat(document.getElementById("cSide").value);
+      this.h=parseFloat(document.getElementById("height").value);
+   
+   }
+   
+   getTriArea(){return (a*this.h)/2};
+   getTriPerim(){return a + b + this.c}
 }
 // menage inputfields display
 const geo = (e) =>{
-   const target = e.target;
-   console.log(target.id);
-      if((target.id="square") || (target.id= "sqimg")){
+   selectedGeoId = e.target.id;
+   areaRes= document.getElementById("AreaResult");
+   perimeterRes= document.getElementById("PerimResult");
+   diagonalRes= document.getElementById("DiagonalResult");
+   //let targetID=target.id;
+  // console.log(targetID);
+ /*   document.getElementById("result").style.display="none";
+   document.getElementById("parameters").style.display="block"; */
+      if((selectedGeoId==="square") || (selectedGeoId=== "sqimg")){
          //define result outputs field
-         areaRes= document.getElementById("AreaResult");
-         perimeterRes= document.getElementById("PerimResult");
-         diagonalRes= document.getElementById("DiagonalResult");
             parA.style.display="block";
-            parB.style.display="none";
-            a= parseFloat(document.getElementById("aSide").value);
-            //make new object
-            const sq= new square(a);
-            //add result to labels
-            let area=sq.getRectArea();
-            let perim=sq.getRectPerim();
-            let diagon=sq.getRectDiag();
-            let geometry="Square"
-
-            areaRes.innerText=`${geometry} Area result: ${area} unit`;
-            perimeterRes.innerText=`${geometry} Perimeter result: ${perim} unit`;
-            diagonalRes.innerText=`${geometry} Diagonal result: ${diagon} unit`;
+            parB.style.display="none";                                            
+            parC.style.display="none";                                            
+            parH.style.display="none";                                            
       }
-      if((target.id="rectangle")||(target.id="rectimg")){
+      if((selectedGeoId==="rectangle") || (selectedGeoId==="rectimg")){         
         //define result outputs field
         areaRes= document.getElementById("AreaResult");
         perimeterRes= document.getElementById("PerimResult");
         diagonalRes= document.getElementById("DiagonalResult");
            parA.style.display="block";
            parB.style.display="block";
-           a= parseFloat(document.getElementById("aSide").value);
-           b= parseFloat(document.getElementById("bSide").value);
-           //make new object
-         const rect= new rectangle();
-         let area=rect.getSqArea();
-         let perimeter=rect.getSqPerim();
-         let geometry="Rectangle"
-
-         areaRes.innerText=`${geometry} Area result: ${area} unit`;
-         perimeterRes.innerText=`${geometry} Perimeter result: ${perim} unit`;
-         diagonalRes.innerText=``;
+           parC.style.display="none";                                            
+           parH.style.display="none"; 
+      }
+      if((selectedGeoId==="triangle") || (selectedGeoId==="triimg")){
+         parA.style.display="block";
+         parB.style.display="block";
+         parC.style.display="block";                                            
+         parH.style.display="block"; 
       }
 }
 
 
+calcGeo=()=>{
+ //let btnID =e.target.id;
+ 
+   if (selectedGeoId==="square" || selectedGeoId ==="sqimg"){
+      
+      a= parseFloat(document.getElementById("aSide").value);
+      const sq= new square(a);
+      //add result to labels
+      let area=sq.getSqArea();
+      let perim=sq.getSqPerim();
+      let diagon=sq.getSqDiag();
+      let geometry="Square";
 
-const rectGeo={
-    a: parseFloat(document.getElementById("aSide").value),
- //   b: parseFloat(document.getElementById("bSide").value),
-  //  getABsum: function(){ return this.a+this.b},
-    getRectArea: function() { return Math.pow(this.a,2)},
-    getRectPrim:function() { return 4*(this.a)},
+      areaRes.innerText=`${geometry} Area result: ${area} unit`;
+      perimeterRes.innerText=`${geometry} Perimeter result: ${perim} unit`;
+      diagonalRes.innerText=`${geometry} Diagonal result: ${diagon} unit`;
+      
+   }
+   if (selectedGeoId==="rectangle" || selectedGeoId ==="rectimg"){
+    //  a= parseFloat(document.getElementById("aSide").value);
+     // b=parseFloat(document.getElementById("bSide").value);
+   //   alert("data a: "+a +"/" +b);
+      const rect= new rectangle();
+         let area=rect.getRectArea();
+         let perim=rect.getRectPerim();
+         let geometry="Rectangle";
+
+         areaRes.innerText=`${geometry} Area result: ${area} unit`;
+         perimeterRes.innerText=`${geometry} Perimeter result: ${perim} unit`;
+         diagonalRes.innerText="";         
+   }
+   if (selectedGeoId==="triangle" || selectedGeoId ==="triimg"){
+      a= parseFloat(document.getElementById("aSide").value);
+      b=parseFloat(document.getElementById("bSide").value);
+      c=parseFloat(document.getElementById("cSide").value);
+      h=parseFloat(document.getElementById("height").value);
+      
+      const tri= new trinagle();
+         let area=tri.getTriArea();
+         let perim=tri.getTriPerim();
+         let geometry="Triangle";
+      
+         areaRes.innerText=`${geometry} Area result: ${area} unit`;
+         perimeterRes.innerText=`${geometry} Perimeter result: ${perim} unit`;
+         diagonalRes.innerText="";         
+   }
+ 
 }
 
 
